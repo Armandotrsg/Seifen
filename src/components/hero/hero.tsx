41fd7b8e4e035804738a3a99dafa69510947a6a1
Armando Terrazas Gómez <A01640924@tec.mx>
2023-05-03 13:06:50 -0600

    Quedó primera sección de Seifen

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,83 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+
+export const Hero = component$(() => {
+    const Badge = ({ badge, text }: { badge: string; text: string }) => {
+        return (
+            <div class={"flex justify-center items-center lg:justify-start"}>
+                <Link
+                    href="#"
+                    class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+                    role="alert"
+                >
+                    <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
+                        {badge}
+                    </span>{" "}
+                    <span class="text-sm font-medium">{text}</span>
+                    <svg
+                        class="ml-2 w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"
+                        ></path>
+                    </svg>
+                </Link>
+            </div>
+        );
+    };
+    return (
+        <>
+            <section class="bg-slate-200 dark:bg-gray-900">
+                <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-[100vh] align-middle">
+                    <div class="mx-auto lg:mr-auto place-self-center lg:col-span-7">
+                        <Badge badge="Nuevo" text="Detergente Seifen" />
+                        <h1 class="max-w-2xl text-center lg:text-start mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                            Seifen
+                        </h1>
+                        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                            Más poder desengrasante y de limpieza, con menos cantidad
+                        </p>
+                        {/* Botones */}
+                        <div class={"flex justify-center lg:justify-start"}>
+                            <a
+                                href="#"
+                                class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+                            >
+                                Conoce más
+                                <svg
+                                    class="w-5 h-5 ml-2 -mr-1"
+                                    fill="currentColor"
+                                    viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                        clip-rule="evenodd"
+                                    ></path>
+                                </svg>
+                            </a>
+                            <a
+                                href="#"
+                                class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+                            >
+                                Contáctanos
+                            </a>
+                        </div>
+                    </div>
+                    <div class="hidden lg:mt-0 lg:col-span-5 lg:flex self-center">
+                        <img
+                            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+                            alt="mockup"
+                        />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Hero } from '~/components/router-head/hero/hero';
+import { Hero } from '~/components/hero/hero';
+import { MainDescription } from '~/components/maindescription/maindescription';
 
 export default component$(() => {
   return (
     <>
       <Hero />
+      <MainDescription />
     </>
   );
 });
