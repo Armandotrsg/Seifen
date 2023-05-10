4d2cd3ed42ed8b71a80813b9cfd110011ba84973
Armando Terrazas Gómez <A01640924@tec.mx>
2023-05-10 17:42:12 -0600

    Intento hacer que funcionen los emails

--- src/components/forms/contact.tsx
+++ src/components/forms/contact.tsx
@@ -1,34 +1,83 @@
-import { component$, useStore } from '@builder.io/qwik';
-import { Form } from '@builder.io/qwik-city';
+import { component$, useStore } from "@builder.io/qwik";
+import { Form } from "@builder.io/qwik-city";
+import { useSendEmail } from "~/routes";
 
 export const Contact = component$(() => {
-  const state = useStore({
-    email: '',
-    subject: '',
-    message: '',
-  });
-//   bg-white dark:bg-gray-900
-  return (
-    <section id='contacto'>
-  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contáctanos</h2>
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">¿Quieres hacer un pedido? Contáctanos para tomarte la orden</p>
-      <Form class="space-y-8">
-          <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email*</label>
-              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="nombre@seifen.com" required onChange$={(e) => state.email = e.target.value} />
-          </div>
-          <div>
-              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Asunto*</label>
-              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Déjanos saber como podemos ayudarte" required onChange$={(e) => state.subject = e.target.value} />
-          </div>
-          <div class="sm:col-span-2">
-              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Mensaje*</label>
-              <textarea id="message" rows={6} class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escribe tu mensaje..." required onChange$={(e) => state.message = e.target.value} />
-          </div>
-          <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
-      </Form>
-  </div>
-</section>
-  );
+    const state = useStore({
+        email: "",
+        subject: "",
+        message: "",
+    });
+    const sendEmail = useSendEmail();
+    return (
+        <section id="contacto">
+            <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+                <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+                    Contáctanos
+                </h2>
+                <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+                    ¿Quieres hacer un pedido? Contáctanos para tomarte la orden
+                </p>
+                <Form action={sendEmail} class="space-y-8">
+                    <div>
+                        <label
+                            for="email"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            Email*
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="nombre@seifen.com"
+                            required
+                            onChange$={(e) => (state.email = e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label
+                            for="subject"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                            Asunto*
+                        </label>
+                        <input
+                            type="text"
+                            id="subject"
+                            name="subject"
+                            class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="Déjanos saber como podemos ayudarte"
+                            required
+                            onChange$={(e) => (state.subject = e.target.value)}
+                        />
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label
+                            for="message"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                        >
+                            Mensaje*
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            rows={6}
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Escribe tu mensaje..."
+                            required
+                            onChange$={(e) => (state.message = e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    >
+                        Send message
+                    </button>
+                </Form>
+            </div>
+        </section>
+    );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,53 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { Hero } from '~/components/hero/hero';
-import { MainDescription } from '~/components/maindescription/maindescription';
-import { CardSection } from '~/components/card/cardsection';
-import { Product } from '~/components/product/product';
-import { Contact } from '~/components/forms/contact';
-import { Faqs } from '~/components/faqs/faqs';
+import { component$ } from "@builder.io/qwik";
+import { type DocumentHead, routeAction$ } from "@builder.io/qwik-city";
+import { Hero } from "~/components/hero/hero";
+import { MainDescription } from "~/components/maindescription/maindescription";
+import { CardSection } from "~/components/card/cardsection";
+import { Product } from "~/components/product/product";
+import { Contact } from "~/components/forms/contact";
+import { Faqs } from "~/components/faqs/faqs";
+
+export const useSendEmail = routeAction$(async (message) => {
+    console.log(message);
+    fetch("https://formsubmit.co/ajax/mandotrsg@gmail.com", {
+      method: "POST",
+      headers: { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+          name: message.subject,
+          message: `Email: ${message.email} \n ${message.message}`
+      })
+  })
+      .then((response) => response.json())
+      .then((data) => {
+          console.log("Success:", data);
+      })
+      .catch((error) => {
+          console.error("Error:", error);
+      });
+});
 
 export default component$(() => {
-  return (
-    <>
-      <Hero />
-      <MainDescription />
-      <CardSection />
-      <Product />
-      <Contact />
-      <Faqs />
-    </>
-  );
+    return (
+        <>
+            <Hero />
+            <MainDescription />
+            <CardSection />
+            <Product />
+            <Contact />
+            <Faqs />
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: 'Seifen',
-  meta: [
-    {
-      name: 'description',
-      content: 'Landing page para Seifen',
-    },
-  ],
+    title: "Seifen",
+    meta: [
+        {
+            name: "description",
+            content: "Landing page para Seifen",
+        },
+    ],
 };
